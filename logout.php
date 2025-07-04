feedb7424fadd8609e3d7e7fe5ebd0168c0cde39
VitorMatheus <71294423+vitin00@users.noreply.github.com>
2025-07-04 02:29:40 -0300

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // limpa as variáveis da sessão
-session_destroy(); // destrói a sessão
-
-header('Location: login.php'); // Redireciona para o login
-exit;
